f85e565287abd88058ca00f8b478b0d1be9f44dc
Peperm00 <74919374+Peperm00@users.noreply.github.com>
2020-12-08 09:34:29 +0100

    Add files via upload

--- img-par.cpp
+++ img-par.cpp
@@ -39,17 +39,23 @@ auto start_sobel = omp_get_wtime();
 auto end_sobel = omp_get_wtime();
 auto end_total = omp_get_wtime();
 
-int width = 0;
-int height = 0;
-int first_byte_old = 0;
-int first_byte_new = 54;
-int file_size_int_new = 0;
+//int width = 0;
+//int height = 0;
+//int first_byte_old = 0;
+const int first_byte_new = 54;
+//int file_size_int_new = 0;
+omp_lock_t writelock;
+omp_lock_t bufflock;
+omp_lock_t testlock;
+omp_lock_t testlock2;
+omp_lock_t gausslock;
+omp_lock_t sobellock;
 
-bool store(char *file_buffer, int file_size_int, ofstream& output_stream);
+bool store(char *file_buffer, int file_size_int, ofstream& output_stream, int first_byte_old);
 
-char* gauss (char *file_buffer, int file_size_int);
+char* gauss (char *file_buffer, int file_size_int, int width, int height, int first_byte_old);
 
-char* sobel(char *file_buffer, int file_size_int);
+char* sobel(char *file_buffer, int file_size_int, int width, int height, int first_byte_old);
 
 void printTime(string operation, string inpath);
 
@@ -95,32 +101,51 @@ int main(int argc, char *argv[]){
 	string outpath;
 	int file_size_int = 0;
 	char *file_buffer;
+	int width = 0;
+	int height = 0;
+	int first_byte_old = 0;
+
+
+	omp_init_lock(&writelock);
+	omp_init_lock(&bufflock);
+	omp_init_lock(&testlock);
+	omp_init_lock(&testlock2);
+	omp_init_lock(&gausslock);
+	omp_init_lock(&sobellock);
 
 	omp_set_num_threads(nthread);
-	#pragma omp parallel private(start_load, end_load, start_store, end_store, start_gauss, end_gauss, start_sobel, end_sobel, end_total, inpath, outpath, file_buffer)
+	#pragma omp parallel private(start_load, end_load, start_store, end_store, start_gauss, end_gauss, start_sobel, end_sobel, end_total, inpath, outpath, file_buffer, file_size_int, height, width, first_byte_old)
 		//for(struct dirent *file = readdir(idir); file != NULL; file = readdir(idir)){
+		//#pragma omp critical
 		while((file = readdir(idir)) != NULL){
+
 		start_load = omp_get_wtime();
 		inpath = indir+"/"+file->d_name;
 		outpath = outdir+"/"+file->d_name;
+
 		ifstream input_stream(inpath, ios::binary);
 
 		if (input_stream.is_open() && inpath != indir+"/"+"." && inpath != indir+"/"+"..")
 		{
+
 				file_size_int = filesystem::file_size(inpath);
 
 				file_buffer = new char[file_size_int];
 
+				omp_set_lock(&bufflock);
 				input_stream.seekg(0, ios::beg);
 				int i = 0;
 				while (input_stream.get(file_buffer[i])){
 					i++;
 				}
+				omp_unset_lock(&bufflock);
 
 				end_load = omp_get_wtime();
 
+
 				if (int(file_buffer[26]) == 1 && int(file_buffer[27]) == 0 && int(file_buffer[28]) == 24 && int(file_buffer[29]) == 0 && int(file_buffer[30]) == 0 &&
 				int(file_buffer[31]) == 0 && int(file_buffer[32]) == 0 && int(file_buffer[33]) == 0){
+
 					ofstream output_stream(outpath, ios::binary);
 
 					first_byte_old = int((unsigned char)file_buffer[13] << 24 |  (unsigned char)file_buffer[12] << 16 |  (unsigned char)file_buffer[11] << 8 |(unsigned char)file_buffer[10]);
@@ -131,20 +156,20 @@ int main(int argc, char *argv[]){
 						if(operation != "copy"){
 
 							start_gauss = omp_get_wtime();
-							file_buffer = gauss(file_buffer, file_size_int);
+							file_buffer = gauss(file_buffer, file_size_int, width, height, first_byte_old);
               end_gauss = omp_get_wtime();
 
 						}
 						if(operation == "sobel"){
 
 							start_sobel = omp_get_wtime();
-							file_buffer = sobel(file_buffer, file_size_int);
+							file_buffer = sobel(file_buffer, file_size_int, width, height, first_byte_old);
               end_sobel = omp_get_wtime();
 
 						}
 
 						start_store = omp_get_wtime();
-						store(file_buffer, file_size_int, output_stream);
+						store(file_buffer, file_size_int, output_stream, first_byte_old);
             end_store = omp_get_wtime();
 
 						output_stream.close();
@@ -155,96 +180,121 @@ int main(int argc, char *argv[]){
 					printTime(operation, inpath);
 
 				}
+
 				input_stream.close();
 		}
+
   }
 	return 0;
 }
 
-bool store (char *file_buffer, int file_size_int, ofstream& output_stream){
-	int width = int((unsigned char)file_buffer[21] << 24  |  (unsigned char)file_buffer[20] << 16 |  (unsigned char)file_buffer[19] << 8 | (unsigned char)file_buffer[18]);
-	int height = int((unsigned char)file_buffer[25] << 24 |  (unsigned char)file_buffer[24] << 16 |  (unsigned char)file_buffer[23] << 8 | (unsigned char)file_buffer[22]);
+bool store (char *file_buffer, int file_size_int, ofstream& output_stream, int first_byte_old){
+	//int width = int((unsigned char)file_buffer[21] << 24  |  (unsigned char)file_buffer[20] << 16 |  (unsigned char)file_buffer[19] << 8 | (unsigned char)file_buffer[18]);
+	//int height = int((unsigned char)file_buffer[25] << 24 |  (unsigned char)file_buffer[24] << 16 |  (unsigned char)file_buffer[23] << 8 | (unsigned char)file_buffer[22]);
+	//int first_byte_old = int((unsigned char)file_buffer[13] << 24 |  (unsigned char)file_buffer[12] << 16 |  (unsigned char)file_buffer[11] << 8 |(unsigned char)file_buffer[10]);
 
-	file_buffer[0] = 'B';
-	file_buffer[1] = 'M';
 
-	cout << file_size_int;
+	int file_size_int_new = file_size_int-abs(first_byte_old-first_byte_new);
+
+
+
+	char * res = new char [file_size_int_new];
+
+
+	#pragma omp critical
+	memcpy(&res[0], &file_buffer[0], first_byte_new);
+	#pragma omp critical
+	memcpy(&res[first_byte_new], &file_buffer[first_byte_old], file_size_int-first_byte_old);
+
+
+	res[0] = 'B';
+	res[1] = 'M';
 
 	//Total size
-	file_buffer[2] = (file_size_int);
-	file_buffer[3] = (file_size_int >> 8);
-	file_buffer[4] = (file_size_int >> 16);
-	file_buffer[5] = (file_size_int >> 24);
+	res[5] = (file_size_int_new >> 24);
+	res[4] = (file_size_int_new >> 16);
+	res[3] = (file_size_int_new >> 8);
+	res[2] = (file_size_int_new >> 0);
+
 
 	//Sets to 0 everything that needs to be 0
 	for(int i = 0; i <= 3; i++){
-		file_buffer[i+6] = 0;
-		file_buffer[i+30] = 0;
-		file_buffer[i+46] = 0;
-		file_buffer[i+50] = 0;
+		res[i+6] = 0;
+		res[i+30] = 0;
+		res[i+46] = 0;
+		res[i+50] = 0;
 	}
 
 	//6, 7, 8, 9
 
 	//Value 54
-	file_buffer[10] = char(54);
-	file_buffer[11] = char(54 >> 8);
-	file_buffer[12] = char(54 >> 16);
-	file_buffer[13] = char(54 >> 24);
+	res[13] = char(first_byte_new >> 24);
+	res[12] = char(first_byte_new >> 16);
+	res[11] = char(first_byte_new >> 8);
+	res[10] = char(first_byte_new >> 0);
 
-	cout << int((unsigned char)file_buffer[13] << 24  |  (unsigned char)file_buffer[12] << 16 |  (unsigned char)file_buffer[11] << 8 | (unsigned char)file_buffer[10]);
+
 
 	//Value 40
-	file_buffer[14] = (40);
-	file_buffer[15] = (40 >> 8);
-	file_buffer[16] = (40 >> 16);
-	file_buffer[17] = (40 >> 24);
+	res[17] = (40 >> 24);
+	res[16] = (40 >> 16);
+	res[15] = (40 >> 8);
+	res[14] = (40 >> 0);
 
 	//Numero de píxeles de ancho aqui 18, 19, 20, 21
 	//Numero de píxeles de alto aqui 22, 23, 24, 25
 
 	//Value 1
-	file_buffer[26] = (1);
-	file_buffer[27] = (1 >> 8);
+	res[26] = (1);
+	res[27] = (1 >> 8);
 
 	//Value 24
-	file_buffer[28] = (24);
-	file_buffer[29] = (24 >> 8);
+	res[28] = (24);
+	res[29] = (24 >> 8);
 
 	//30, 31, 32, 33
 
+	int img_size = file_size_int_new - 54;
 	//Size of the image
-	file_buffer[34] = (width*height*3);
-	file_buffer[35] = (width*height*3 >> 8);
-	file_buffer[36] = (width*height*3 >> 16);
-	file_buffer[37] = (width*height*3 >> 24);
+	res[34] = (img_size);
+	res[35] = (img_size >> 8);
+	res[36] = (img_size >> 16);
+	res[37] = (img_size >> 24);
 
 	//Value 2835
-	file_buffer[38] = char(2835);
-	file_buffer[39] = char(2835 >> 8);
-	file_buffer[40] = char(2835 >> 16);
-	file_buffer[41] = char(2835 >> 24);
+	res[38] = char(2835);
+	res[39] = char(2835 >> 8);
+	res[40] = char(2835 >> 16);
+	res[41] = char(2835 >> 24);
 
 	//Value 2835
-	file_buffer[42] = char(2835);
-	file_buffer[43] = char(2835 >> 8);
-	file_buffer[44] = char(2835 >> 16);
-	file_buffer[45] = char(2835 >> 24);
+	res[42] = char(2835);
+	res[43] = char(2835 >> 8);
+	res[44] = char(2835 >> 16);
+	res[45] = char(2835 >> 24);
 
 	//46, 47, 48, 49
 	//50, 51, 52, 53
 
-	if(output_stream.write(file_buffer, file_size_int)){
+	omp_set_lock(&writelock);
+	cout << int((unsigned char)res[5] << 24 |  (unsigned char)res[4] << 16 |  (unsigned char)res[3] << 8 |(unsigned char)res[2]) << "\n";
+	if(output_stream.write(res, file_size_int_new)){
+		omp_unset_lock(&writelock);
 		return true;
 	}
+	omp_unset_lock(&writelock);
 	return false;
 }
 
-char* gauss (char *file_buffer, int file_size_int){
+char* gauss (char *file_buffer, int file_size_int, int width, int height, int first_byte_old){
 
+	omp_set_lock(&gausslock);
 	char *res = new char [file_size_int];
+	omp_unset_lock(&gausslock);
 	#pragma omp critical
 	memcpy(res, file_buffer, file_size_int);
+
+
 
 	for(int i = 0; i < height; i++){
 		for(int j = 0; j < width*3; j+=3){
@@ -262,19 +312,22 @@ char* gauss (char *file_buffer, int file_size_int){
 			res[i*width*3 + j + first_byte_old + 1] = x2/w;
 			res[i*width*3 + j + first_byte_old + 2] = x3/w;
 		}
+
 	}
+
 	return res;
 }
 
 
 
-char* sobel(char *file_buffer, int file_size_int){
+char* sobel(char *file_buffer, int file_size_int, int width, int height, int first_byte_old){
 
+	omp_set_lock(&sobellock);
 	char *res = new char[file_size_int];
-	#pragma omp critical
 	memcpy(res, file_buffer, file_size_int);
 	char *resx = new char[file_size_int];
 	char *resy = new char[file_size_int];
+	omp_unset_lock(&sobellock);
 
 	for(int i = 0; i < height; i++){
 		for(int j = 0; j < width*3; j+=3){
@@ -315,6 +368,7 @@ char* sobel(char *file_buffer, int file_size_int){
 
 		}
 	}
+
 	return file_buffer;
 }
 
